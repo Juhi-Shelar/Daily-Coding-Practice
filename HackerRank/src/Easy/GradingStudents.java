bf48a837ca17c194cf93626ceffd170c29e91868
Juhi-Shelar <juhishelar@gmail.com>
2019-11-01 22:42:02 +0530

    HackerRank Initial commit

---
+++ HackerRank/src/Easy/GradingStudents.java
@@ -0,0 +1,62 @@
+package Easy;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.joining;
+
+public class GradingStudents {
+    public static List<Integer> gradingStudents(List<Integer> grades) {
+    	List<Integer> res = new ArrayList<Integer>();
+    	
+		for (int temp : grades) {
+			if(temp < 38)
+				res.add(temp);
+			else if( temp%5 >= 3)
+				res.add(temp+(5-(temp%5)));
+			else
+				res.add(temp);
+		}
+    	
+    	return res;
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int gradesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> grades = IntStream.range(0, gradesCount).mapToObj(i -> {
+            try {
+                return bufferedReader.readLine().replaceAll("\\s+$", "");
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        })
+            .map(String::trim)
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> result = GradingStudents.gradingStudents(grades);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining("\n"))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+    
+}
+
