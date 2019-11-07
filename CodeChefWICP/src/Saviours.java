415c4e98f1f2f9cc0caf5994f2479d5e6c0bd223
Juhi-Shelar <juhishelar@gmail.com>
2019-11-07 21:41:12 +0530

    updated code

--- CodeChefWICP/src/Saviours.java
+++ CodeChefWICP/src/Saviours.java
@@ -1,76 +1,66 @@
-import java.lang.*;
+
 import java.util.*;
 
-public class Saviours{
+public class Saviours
+{
 
      public static void main(String []args){
         Scanner s=new Scanner(System.in);
-        int t=0;
+        int t=s.nextInt();
         
-        t=s.nextInt();
-        
-        String res[]= new String[t];
-        int ind=0;
-        
+        //s.nextLine();
         for(int j=0;j<t;j++)
         {
         String str=s.next();
-        
         int len=str.length();
         char[] ch=new char[str.length()];
+         ArrayList<Integer> arr=new ArrayList<Integer>();
         ch=str.toCharArray();
         boolean stat=false;
-        
-        ArrayList<Integer> arr=new ArrayList<Integer>();
-        if(ch[0]=='0'){
-            res[ind]="NO";
-            ind++;
+       
+        if(ch[0]=='0')
+        {
+            System.out.println("NO");
             stat=true;
+           
         }
        else if(str.substring(len-3,len).equals("000"))
         {
-    	   res[ind]="NO";
-           ind++;
-           stat=true;
+            System.out.println("NO");
+            stat=true;
+           
         }
        else{
+        //System.out.println("here");
         
            for(int i=0;i<ch.length;i++)
            {
                if(ch[i]=='*')
-               {  
-                   int l=arr.lastIndexOf('*');
+               {   if(arr.size()>=1)
+                  {
+                   int l=arr.get(arr.size()-1);
+                   //System.out.println(l);
                    if(l!=-1)
-                   {
+                   { //System.out.println(l+"l"+i+"i");
                        if(i-l>3)
                        {
-                    	   res[ind]="NO";
-                           ind++;
-                           
-                           stat=true;
+                           System.out.println("NO");
+                            stat=true;
                             break;
                        }
-                   }
+                   }}
                    arr.add(i);
+                   //System.out.println(arr);
                    
                }
            }
-           
               
            }
            if(stat==false)
            {
-               
-        	   res[ind]="YES";
-               ind++;
-               
+               System.out.println("YES");
            }
         
      }
-        
-        
-        for(int i=0;i<ind;i++)
-        	System.out.println(res[i]);
-        	
 }
 }
